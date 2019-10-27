aae53364db3b1957bd2c1e965a023f4a146b5900
omoshiroi <robertkwalshuk@gmail.com>
2019-10-27 10:59:52 -0400

    initial commit

---
+++ test-project/models/test_table.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
+SELECT 
+    customer_id, 
+    current_timestamp AS date_time 
+FROM 
+    dev_rwalsh.customer_orders
+
+{% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    WHERE customer_id > (select min(customer_id) from {{this}})
+{% endif %}
