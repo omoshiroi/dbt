f6f701f386ac6590e6106b84d3ee3d18109b9b99
omoshiroi <robertkwalshuk@gmail.com>
2019-11-03 16:10:27 -0500

    Added test_agg to schema yml, and added inc load

--- test-project/models/test_agg.sql
+++ test-project/models/test_agg.sql
@@ -1,5 +1,16 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
 SELECT 
     COUNT(*) AS qty,
     current_timestamp AS date_time
 FROM
     {{ref('test_table')}}
+
+{% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    WHERE current_timestamp > (select max(date_time) from {{this}})
+{% endif %}
